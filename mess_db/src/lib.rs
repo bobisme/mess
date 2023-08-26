70397b26d8619dfe527a0fcd2b530a9bfafe6d5e
Bob <bob@standardinput.co>
2023-08-26 18:11:06 -0400

    mess_db: add 63-bit stream pos representation

--- mess_db/src/lib.rs
+++ mess_db/src/lib.rs
@@ -6,6 +6,41 @@ pub mod rusqlite;
 pub mod sqlx;
 pub mod write;
 
+/// StreamPos uses a 63-bit uint for representing position and
+/// a u64 for storing the value with a 1-bit flag. This allows
+/// the system to tell immediately whether the stream is using
+/// strict Serial ordering or Causal ordering via the use of
+/// a hybrid logical clock.
+#[derive(Clone, Copy, Debug, PartialEq, PartialOrd)]
+pub enum StreamPos {
+    Serial(u64),
+    Causal(u64),
+}
+
+impl StreamPos {
+    pub fn to_store(self) -> u64 {
+        match self {
+            StreamPos::Serial(pos) => pos << 1,
+            StreamPos::Causal(pos) => (pos << 1) | 1,
+        }
+    }
+
+    pub fn from_store(stored_position: u64) -> Self {
+        match stored_position & 1 {
+            0 => Self::Serial(stored_position >> 1),
+            1 => Self::Causal(stored_position >> 1),
+            _ => unreachable!(),
+        }
+    }
+
+    // Returns the 63-bit position (as u64).
+    pub fn position(self) -> u64 {
+        match self {
+            Self::Serial(pos) | Self::Causal(pos) => pos,
+        }
+    }
+}
+
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub struct Position {
     pub global: u64,
@@ -46,5 +81,49 @@ impl TryFrom<&::rusqlite::Row<'_>> for Message {
             metadata: row.get(6)?,
             id: row.get(7)?,
         })
+    }
+}
+
+#[cfg(test)]
+mod test_stream_pos {
+    use super::*;
+    use assert2::assert;
+
+    use StreamPos::{Causal, Serial};
+
+    #[rstest::rstest]
+    #[case(Serial(1))]
+    #[case(Serial(1 << 48))]
+    #[case(Causal(1))]
+    #[case(Causal(1 << 48))]
+    fn position_returns_the_actual_position(#[case] input: StreamPos) {
+        let expected = match input {
+            Serial(pos) | Causal(pos) => pos,
+        };
+        assert!(input.position() == expected);
+    }
+
+    #[rstest::rstest]
+    #[case(Serial(1), 2)]
+    #[case(Serial(1 << 48), 1 << 49)]
+    #[case(Causal(1), 3)]
+    #[case(Causal(1 << 48), (1 << 49) + 1)]
+    fn to_store_shifts_and_sets_bit(
+        #[case] input: StreamPos,
+        #[case] expected: u64,
+    ) {
+        assert!(input.to_store() == expected)
+    }
+
+    #[rstest::rstest]
+    #[case(2, Serial(1))]
+    #[case(1 << 49, Serial(1 << 48))]
+    #[case(3, Causal(1))]
+    #[case((1 << 49) + 1, Causal(1 << 48))]
+    fn from_store_shifts_and_reads_bit(
+        #[case] input: u64,
+        #[case] expected: StreamPos,
+    ) {
+        assert!(StreamPos::from_store(input) == expected)
     }
 }
